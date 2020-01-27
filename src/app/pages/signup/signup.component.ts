0adf2a4fa0da62f0bbc5556c685a55951fcb03c9
mgarreaza13 <mg.arreaza.13@gmail.com>
2020-01-27 16:51:16 -0400

    register

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,16 +1,86 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { User } from 'src/app/shared/models/user';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Router } from '@angular/router';
+import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.component.html',
     styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-    test : Date = new Date();
+    test: Date = new Date();
     focus;
     focus1;
     focus2;
-    constructor() { }
+    public registerForm: FormGroup;
+    public user: User = {};
+    public submitted = false;
 
-    ngOnInit() {}
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private lsService: LocalStorageService,
+        private router: Router) { }
+
+    ngOnInit() {
+        // Validate token
+        const token = this.lsService.getValue('token');
+        if (token) {
+            this.router.navigate(['/']);
+        }
+
+        // Build form
+        this.registerForm = this.formBuilder.group({
+            username: ['', [
+                Validators.required,
+            ]],
+            first_name: ['', [
+                Validators.required,
+            ]],
+            last_name: ['', [
+                Validators.required,
+            ]],
+            email: ['', [
+                Validators.required,
+            ]],
+            password1: ['', [
+                Validators.required,
+                Validators.minLength(6),
+                Validators.maxLength(30),
+            ]],
+            password2: ['', [
+                Validators.required,
+                Validators.minLength(6),
+                Validators.maxLength(30),
+            ]],
+        });
+    }
+
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        // Set object
+        this.user.username = this.registerForm.get('username').value;
+        this.user.first_name = this.registerForm.get('first_name').value;
+        this.user.last_name = this.registerForm.get('last_name').value;
+        this.user.email = this.registerForm.get('email').value;
+        this.user.password1 = this.registerForm.get('password1').value;
+        this.user.password2 = this.registerForm.get('password2').value;
+
+        // Send request
+        this.authService.register(this.user).subscribe(
+            (data: any) => {
+                this.router.navigateByUrl('/login')
+            },
+            err => { console.log(err);}
+        );
+    }
+
 }
